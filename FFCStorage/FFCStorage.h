2ca75d47902c6e565bb315cd267ca1d5bf4887f5
Fabian Canas <fcanas@gmail.com>
2015-02-21 11:12:33 -0500

    Initial Commit

---
+++ FFCStorage/FFCStorage.h
@@ -0,0 +1,19 @@
+//
+//  FFCStorage.h
+//  FFCStorage
+//
+//  Created by Fabian Canas on 2/21/15.
+//  Copyright (c) 2015 Fabian Canas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FFCStorage.
+FOUNDATION_EXPORT double FFCStorageVersionNumber;
+
+//! Project version string for FFCStorage.
+FOUNDATION_EXPORT const unsigned char FFCStorageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FFCStorage/PublicHeader.h>
+
+
